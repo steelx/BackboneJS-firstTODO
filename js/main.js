9302247e7fc01bade64f7d36988a8ee9bb708450
Ajinkya <ajinkyab1987@gmail.com>
2014-02-26 19:58:55 +0530

    Added collections and views. UL loops thru each loop LI

--- js/main.js
+++ js/main.js
@@ -12,23 +12,53 @@
 
 	App.Models.Task = Backbone.Model.extend({});
 
+	App.Collections.Tasks = Backbone.Collection.extend({
+		model: App.Models.Task
+	});
+
+	App.Views.Tasks = Backbone.View.extend({
+		tagName: 'ul',
+
+		render: function () {
+			this.collection.each(this.addOne, this);
+			return this;
+		},
+
+		addOne: function (task) {
+			//this receives single model ie. task
+			var taskView = new App.Views.Task({ model: task });
+
+			//append to root Tasks UL element
+			this.$el.append( taskView.render().el );
+		}
+	});
+
 	App.Views.Task = Backbone.View.extend({
 		tagName: 'li',
 
 		render: function () {
-			var source = template('taskTemplate')
-			this.$el.html(source(this.model.attributes));
+			this.$el.html( this.model.get('title') );
 			return this;
 		}
 	});
 
-	var task = new App.Models.Task({
-		title: "Get going buddy",
-		priority: 5
-	});
+	var tasks = new App.Collections.Tasks([
+		{
+			title: "Get BackboneJS",
+			priority: 5
+		},
+		{
+			title: "Punch in LoDash",
+			priority: 4
+		},
+		{
+			title: "NodeJS on the way",
+			priority: 3
+		}
+	]);
 
-	var taskView = new App.Views.Task({ model: task });
+	var tasksView = new App.Views.Tasks({ model: tasks });
 
-	$('.tasks').html(taskView.render().el);
+	$('.tasks').html(tasksView.render().el);
 
 })();
